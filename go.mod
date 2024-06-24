d1928c80c6d85c53891766000a01ab459f2f6f29
Nathan G. Santos <nathangar01@gmail.com>
2024-06-23 22:57:31 -0300

    refact: using only one value by node

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NathanGds/binary-tree
 
 go 1.20
+
+require github.com/m1gwings/treedrawer v0.3.3-beta // indirect
--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+type Node struct {
+	left  *Node
+	right *Node
+	value int
+}
+
+func (n *Node) insert(value int) {
+	if n.value == 0 {
+		n.insertFirstValue(value)
+		return
+	}
+}
+
+func (n *Node) insertFirstValue(value int) {
+	n.value = value
+}
 
 func main() {
-	fmt.Println("start project")
+	var node Node
+	node.insert(2)
 }
