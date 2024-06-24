c9e9e43ffa3b14874ca0e95e82eb591635b96c0a
Nathan G. Santos <nathangar01@gmail.com>
2024-06-23 23:03:55 -0300

    feat: insert fest childs

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/m1gwings/treedrawer/tree"
+)
+
 type Node struct {
 	left  *Node
 	right *Node
@@ -11,6 +17,17 @@ func (n *Node) insert(value int) {
 		n.insertFirstValue(value)
 		return
 	}
+
+	if value < n.value {
+		n.left = &Node{left: nil, right: nil, value: value}
+		return
+	}
+
+	if value > n.value {
+		n.right = &Node{left: nil, right: nil, value: value}
+		return
+	}
+
 }
 
 func (n *Node) insertFirstValue(value int) {
@@ -19,5 +36,13 @@ func (n *Node) insertFirstValue(value int) {
 
 func main() {
 	var node Node
-	node.insert(2)
+	node.insert(5)
+	node.insert(4)
+	node.insert(8)
+
+	t := tree.NewTree(tree.NodeInt64(node.value))
+	t.AddChild(tree.NodeInt64(node.left.value))
+	t.AddChild(tree.NodeInt64(node.right.value))
+
+	fmt.Println(t)
 }
